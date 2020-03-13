db7e292e1387ab88ea49bfaedbc8409ee6f17da6
Robson <robsonvs@gmail.com>
2020-03-12 22:26:09 -0400

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
-import { UserComponent } from './user/user/user.component';
-
+import { UserFormComponent } from './user/user-form/user-form.component';
+import { PopListComponent } from './pop/pop-list/pop-list.component';
+import { PopFormComponent } from './pop/pop-form/pop-form.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'users', pathMatch: 'full' },
-  { path: 'users', component: UserListComponent },
-  { path: 'add', component: UserComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: PopListComponent },
+  { path: 'formUser', component: UserFormComponent },
+  { path: 'listUser', component: UserListComponent },
+  { path: 'formPop', component: PopFormComponent },
+  { path: 'listPop', component: PopListComponent }
 ];
 
 @NgModule({
